fd2c8981f53ebdfaf6a807519e48123df76e871e
Juan <juanfridano@gmail.com>
2016-09-16 17:00:16 +0200

    2

--- src/controllers/App.java
+++ src/controllers/App.java
@@ -1,13 +1,14 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 
-import data.solr.Product;
 import data.solr.ProductRepository;
 
 @SpringBootApplication
@@ -15,7 +16,7 @@ import data.solr.ProductRepository;
 @EnableSolrRepositories("data.solr.ProductRepository")
 public class App implements CommandLineRunner {
 
-	// @Autowired
+	@Autowired
 	private ProductRepository repository;
 
 	public static void main(String[] args) throws Exception {
@@ -23,6 +24,7 @@ public class App implements CommandLineRunner {
 		SpringApplication.run(App.class, args);
 	}
 
+	SolrTemplate st;
 
 	@Bean
 	public String solrTemplate() {
@@ -33,27 +35,7 @@ public class App implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("juhuu!!!");
-		this.repository.deleteAll();
-
-		// insert some products
-		this.repository.save(new Product("1", "Nintendo Entertainment System"));
-		this.repository.save(new Product("2", "Sega Megadrive"));
-		this.repository.save(new Product("3", "Sony Playstation"));
-
-		// fetch all
-		System.out.println("Products found by findAll():");
-		System.out.println("----------------------------");
-		for (Product product : this.repository.findAll()) {
-			System.out.println(product);
-		}
-		System.out.println();
-
-		// fetch a single product
-		System.out.println("Products found with findByNameStartingWith('So'):");
-		System.out.println("--------------------------------");
-		for (Product product : this.repository.findByNameStartingWith("So")) {
-			System.out.println(product);
-		}
-		System.out.println();
+		
+		//repository.toString();
 	}
 }
--- src/data/solr/ProductRepository.java
+++ src/data/solr/ProductRepository.java
@@ -3,9 +3,11 @@ package data.solr;
 import java.util.List;
 
 import org.springframework.data.solr.repository.SolrCrudRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Component
+@Service 
+@Transactional
 public interface ProductRepository extends SolrCrudRepository<Product, String> {
 
 	List<Product> findByNameStartingWith(String name);
